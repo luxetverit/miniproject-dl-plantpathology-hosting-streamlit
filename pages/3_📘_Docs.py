741ad8ed514e42b6b39ff4441ab588168c5c8268
jwnam <replaysoccer@naver.com>
2023-01-30 09:56:36 +0900

    update code

--- pages/3_📘_Docs.py
+++ pages/3_📘_Docs.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='4leaf',
+    layout='centered',
+)
+
+st.header('Code Viewer🧱')
+
 if st.button('app.py code view'):
     code = '''
         st.text('enter code🍀')
